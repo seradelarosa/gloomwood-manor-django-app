6494c82c81d3738d07a6747fc74ea72a369865ef
Sera De La Rosa <sera.delarosa@outlook.com>
2025-04-08 13:00:14 -0400

    Fixed reverse query for models.

--- main_app/models.py
+++ main_app/models.py
@@ -8,8 +8,9 @@ class Room(models.Model):
     tile_position = models.JSONField()
     # what the room costs in whole numbers 
     value = models.IntegerField()
-    guest = models.ForeignKey('Guest', null=True, blank=True, on_delete=models.SET_NULL)
-    ghost = models.ForeignKey('Ghost', null=True, blank=True, on_delete=models.SET_NULL)
+    # set reverse query names
+    guest = models.ForeignKey('Guest', null=True, blank=True, on_delete=models.SET_NULL, related_name='room_guest')
+    ghost = models.ForeignKey('Ghost', null=True, blank=True, on_delete=models.SET_NULL, related_name='room_ghost')
 
 class Guest(models.Model):
     full_name = models.CharField(max_length=100)
@@ -17,14 +18,14 @@ class Guest(models.Model):
     stay_duration = models.IntegerField()  
     preferences = models.TextField(blank=True)
     registered = models.BooleanField(default=False)
-    assigned = models.ForeignKey(Room, null=True, blank=True, on_delete=models.SET_NULL)
+    assigned = models.ForeignKey(Room, null=True, blank=True, on_delete=models.SET_NULL, related_name='assigned_guests')
     # useful for our timers later
     check_in_time = models.DateTimeField(null=True, blank=True)
 
 class Ghost(models.Model):
     name = models.CharField(max_length=100)
     ghost_type = models.CharField(max_length=50)
-    assigned = models.ForeignKey(Room, null=True, blank=True, on_delete=models.SET_NULL)
+    assigned = models.ForeignKey(Room, null=True, blank=True, on_delete=models.SET_NULL, related_name='assigned_ghosts')
     # useful for our timers later
     check_in_time = models.DateTimeField(null=True, blank=True)
 
