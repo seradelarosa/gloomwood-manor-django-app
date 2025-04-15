82aa52e52966b8b9964917f6c3ae63d2ff0b72ce
Sera De La Rosa <sera.delarosa@outlook.com>
2025-04-15 00:21:54 -0400

    configured django to serve react index.html

--- gloomwoodmanor/settings.py
+++ gloomwoodmanor/settings.py
@@ -91,7 +91,7 @@ ROOT_URLCONF = 'gloomwoodmanor.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'main_app/static')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -171,6 +171,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'main_app/static'),
+]
 
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
--- main_app/urls.py
+++ main_app/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from . import views
+from django.views.generic import TemplateView
 
 from rest_framework.routers import DefaultRouter
 from .views import RoomViewSet, GuestViewSet, GhostViewSet, assign_room_api, GuestListView, UnregisteredGuestListView
@@ -11,8 +12,8 @@ router.register(r'guests', GuestViewSet)
 router.register(r'ghosts', GhostViewSet)
 
 urlpatterns = [
-    path('api/', include(router.urls)),
     path('admin/', admin.site.urls),
+    path('api/', include(router.urls)),
     path('', views.Home.as_view(), name='home'),
     # endpoints for guests
     path('api/guests/', GuestListView.as_view(), name='guest-list'),
@@ -23,5 +24,7 @@ urlpatterns = [
     path('api/login/', views.LoginAPI.as_view(), name='login_api'),
     path('api/get-csrf-token/', views.get_csrf_token, name='get_csrf_token'),
     path('api/csrf/', views.get_csrf_token, name='csrf_token'),
+    # Catch-all route for React frontend
+    re_path(r'^.*$', TemplateView.as_view(template_name='index.html')),
 ]
 
